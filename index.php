43fd59dcf46dcaf4a54afaac3485548474b522bf
Nikita <nerdyaus21@gmail.com>
2023-11-03 21:45:37 +0300

    forms makes a request to itself

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-$dishes = array('milk', 'bread');
-print_r ($dishes);
+if ('POST' == $_SERVER['REQUEST_METHOD']){
+    print "Hello, " . $_POST['my_name'];
+} else{
+    print<<<_HTML_
+<form method="post" action="$_SERVER[PHP_SELF]">
+Your name: <input type="text" name="my_name">
+<br>
+<input type="submit" value="Send">
+</form>
+_HTML_;
+
+}
