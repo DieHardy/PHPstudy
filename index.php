56884d3ff46afcc986b5f3f7d97461ab1e56cddc
Nikita <nerdyaus21@gmail.com>
2023-11-04 08:57:38 +0300

    updated menu

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 
 <?php
+echo "<ul>";
+for($i=1; $i < 6; $i++){
+    echo "<li><a href='./ex$i'>Exercise $i</a></li>";
+}
+echo "</ul>";
 ?>
 
-<ul>
-    <li><a href="./ex1">Exercise 1</a></li>
-    <li><a href="./ex1">Exercise 2</a></li>
-    <li><a href="./ex1">Exercise 3</a></li>
-    <li><a href="./ex1">Exercise 4</a></li>
 
-</ul>
