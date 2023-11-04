bb69b45d076ad9e6cf2c64b71addd117eff10784
Nikita <nerdyaus21@gmail.com>
2023-11-04 10:25:03 +0300

    added a minimum validation  proccess

--- ex1/functions.php
+++ ex1/functions.php
@@ -2,7 +2,12 @@
 function process_form(){
     print "Hello, " . $_POST['my_name'];
 }
-function print_form(){
+function print_form($errors =''){
+    if($errors){
+        print 'Please correct these errors: <ul><li>';
+        print implode('</li></li>', $errors);
+        print '</li></ul>';
+    }
     print<<<_HTML_
 <form method="post" action="$_SERVER[PHP_SELF]">
     Your name: <input type="text" name="my_name">
@@ -12,9 +17,12 @@ function print_form(){
 _HTML_;
 }
 function validate_form(){
- if(strlen($_POST['my_name']) < 3){
-     return false;
- }   else{
-     return true;
+    $errors = array();
+ if(strlen(trim($_POST['my_name'])) < 3){
+     $errors[] = 'Your name must be at least 3 letters long.';
  }
+ if($_POST['my_name'] == 'administrator'){
+     $errors[] = 'You can`t be an administrator';
+ }
+ return $errors;
 }
--- ex1/index.php
+++ ex1/index.php
@@ -1,12 +1,11 @@
-<?php
+<?php // the form makes a request to itself
 require 'functions.php';
-// the form makes a request to itself
 if ('POST' == $_SERVER['REQUEST_METHOD']){
-    if(validate_form()){
-    process_form();
-    }
-    else{
-        print_form();
+    // if an array of errors is empty then print username
+    if($form_errors = validate_form()){
+    print_form($form_errors);
+    }  else{
+        process_form();
     }
 } else{
 print_form();
