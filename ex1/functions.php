91bce39e360ef28f79a226b970b35723ad577fb4
Nikita <nerdyaus21@gmail.com>
2023-11-04 12:19:41 +0300

    added age

--- ex1/functions.php
+++ ex1/functions.php
@@ -1,7 +1,8 @@
 <?php
 function process_form(){
     print "Hello, " . $_POST['my_name'] . "<br>";
-    print "Your email: " . $_POST['email'];
+    print "Your email: " . $_POST['email'] . "<br>";
+    print "Your age: " . $_POST['age'];
 }
 function print_form($errors =''){
     if($errors){
@@ -14,20 +15,30 @@ function print_form($errors =''){
     Your name: <input type="text" name="my_name">
     <br>
     Your email: <input type="email" name="email">
+        <br>
+    Your age: <input type="text" name="age">
     <input type="submit" value="Send">
 </form>
 _HTML_;
 }
 function validate_form(){
     $errors = array();
- if(strlen(trim($_POST['my_name'])) < 3){
+ if(3 > strlen(trim($_POST['my_name'])) ){
      $errors[] = 'Your name must be at least 3 letters long.';
  }
- if($_POST['my_name'] == 'administrator'){
-     $errors[] = 'You can`t be an administrator';
+ if('administrator' == $_POST['my_name'] ){
+     $errors[] = 'You can`t be an administrator.';
  }
- if(strlen($_POST['email']) == 0){
-     $errors[] = "You have to enter an email!";
+ if(0 == strlen($_POST['email']) ){
+     $errors[] = "You have to enter an email.";
  }
+ if(!is_null($_POST['age']) || filter_input(INPUT_POST, 'age', FILTER_VALIDATE_INT)){
+     if(18 > $_POST['age']){
+         $errors[] = "You can`t be less than 18.";
+     }
+ } else{
+     $errors[] = "Please enter a valid age.";
+ }
+
  return $errors;
 }
